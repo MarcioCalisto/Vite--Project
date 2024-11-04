313f1b95003688113da1fcc8ae51b8ecfb0b1c89
unknown <seradmin@FMN009-L002-006.serlab.intra>
2024-11-04 20:34:18 -0300

    Commit

---
+++ vite-quest/vite-project/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [dado, setDado] = useState({
+    nome: "Márcio",
+    especie: "Puto Belga",
+    genero: "delu",
+  })
+
+  const [dadosAPI, setDadosAPI] = useState([
+    dado, {
+      nome: "Jubileu",
+      especie: "Homosexuel",
+      genero: "Elu/Omo"
+    }, {
+      nome: "Picapau",
+      especie: "Homosapies",
+      genero: "Mata alta"
+    }
+  ])
+
+  return (
+    <>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>{dado.nome}</td>
+            <td>{dado.especie}</td>
+            <td>{dado.genero}</td>
+          </tr>
+          {
+            dadosAPI.map((dado, index) => {
+              return (
+                <tr>
+                  <th scope="row">{index}</th>
+                  <td>{dado.nome}</td>
+                  <td>{dado.especie}</td>
+                  <td>{dado.genero}</td>
+                </tr>)
+            })
+          }
+          <tr>
+            <th scope="row">3</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
